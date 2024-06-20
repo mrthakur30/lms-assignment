71b72e99df25f2930f4e7bbe295c5def9cb2cf89
mrthakur30 <21bec049@nith.ac.in>
2024-06-20 18:40:08 +0530

    done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Admin from './pages/Admin';
 import Reports from './pages/Reports';
 import Maintenance from './pages/Maintenance';
